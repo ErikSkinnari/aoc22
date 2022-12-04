8f6c0b9057c9818a3e6f926496d6820bcd8e88ff
Erik Skinnari <erik@skinnari.se>
2022-12-05 00:52:32 +0100

    day 3 part 1

--- 03/day3.py
+++ 03/day3.py
@@ -1,2 +1,23 @@
+itemCollection = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+priorityScore = 0
 fileContent = open("input.txt", "r")
+
+def getFirstCompartment(rucksack):
+    return rucksack[0:len(rucksack)//2]
+
+def getSecondCompartment(rucksack):
+    return rucksack[len(rucksack)//2:]
+
+def getPriorityScore(item):
+    return itemCollection.index(item) + 1
+
+def findCommonItem(compartmentOne, compartmentTwo):
+    return ''.join(set(compartmentOne).intersection(compartmentTwo))
+
+
+for rucksack in fileContent:
+    commonItem = findCommonItem(getFirstCompartment(rucksack), getSecondCompartment(rucksack))
+    priorityScore += getPriorityScore(commonItem)
+
+print(priorityScore)
 
