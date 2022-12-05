f126d4559566e14901ce807fc4e06d90f8b390bb
Erik Skinnari <erik@skinnari.se>
2022-12-05 08:11:54 +0100

    day 4 part 1

--- 04/day4.py
+++ 04/day4.py
@@ -1,1 +1,27 @@
 input = open("input.txt", "r")
+
+completeContainsCounter = 0
+
+def rangeIsContained(range1x, range1y, range2x, range2y):
+    if ((range1x <= range2x and range1y >= range2y) 
+            or (range2x <= range1x and range2y >= range1y)):
+        return True
+    return False
+
+
+for line in input:
+
+    range1 = line.split(',')[0]
+    range2 = line.split(',')[1]
+
+    range1x = int(range1.split('-')[0])
+    range1y = int(range1.split('-')[1])
+    range2x = int(range2.split('-')[0])
+    range2y = int(range2.split('-')[1])
+
+    if rangeIsContained(range1x, range1y, range2x, range2y):
+        completeContainsCounter += 1
+
+print(completeContainsCounter)
+
+
