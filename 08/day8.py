4cf068dbeacb899607480b60e7f4c82ebf480d0b
Erik Skinnari <erik@skinnari.se>
2022-12-10 21:28:14 +0100

    solve day 8

--- 08/day8.py
+++ 08/day8.py
@@ -4,82 +4,147 @@ forrest = []
 
 for line in filecontent:
     forrest.append(line.replace("\n", ""))
+forrestHeight = len(forrest)
+forrestWidth = len(forrest[0])
 
-def isHiddenBehindAnotherTree(height, otherTrees):
-    for tree in otherTrees:
-        if int(tree) >= int(height):
-            return True
-    return False
+class Tree:
+    def __init__(self, x, y, height):
+        self.x = x
+        self.y = y
+        self.height = int(height)
 
+    def getScenicScore(self):
+        if (self.x == 0 or self.y == 0 or self.x == forrestWidth - 1 or self.y == forrestHeight - 1):
+            return 0
+        west = calculateScore(self.height, getWesternTrees(self.x, self.y))
+        north = calculateScore(self.height, getNorthernTrees(self.x, self.y))
+        east = calculateScore(self.height, getEasternTrees(self.x, self.y))
+        south = calculateScore(self.height, getSouthernTrees(self.x, self.y))
+
+        calculatedScore = west * north * east * south
+        return calculatedScore
+
+
+def reverseString(string):
+    return string[::-1]
+
+def calculateScore(height, treeLine):
+    score = 0
+    for i in range(len(treeLine)):
+        score += 1
+        if int(treeLine[i]) >= int(height):
+            break
+    return int(score)
+
+### Get list of trees in a direction ###
+def getWesternTrees(x, y):
+    westernTrees = forrest[y][0:x]
+    return reverseString(westernTrees)
+
+def getNorthernTrees(x, y):
+    verticalTrees = getVerticalTrees(x)
+    return reverseString(verticalTrees[0: y])
+
+def getEasternTrees(x, y):
+    return forrest[y][x + 1:]
+
+def getSouthernTrees(x, y):
+    return getVerticalTrees(x)[y + 1:]
+
+
+### Get trees in a vertical line by x value ###
 def getVerticalTrees(x):
-    trees = []
+    trees = ""
     for treeline in forrest:
-        trees.append(treeline[x])
+        trees += treeline[x]
     return trees
 
+def getTreeHeight(x, y):
+    return forrest[y][x]
+
+
+### Calculate visibility functions ####
 def isVisibleEast(x, y):
-    easternTrees = forrest[y]
-    if x == len(easternTrees) - 1:
+    easternTrees = getEasternTrees(x, y)
+    if x == forrestWidth - 1:
         return True
-    return isHiddenBehindAnotherTree(easternTrees[x], easternTrees[x + 1:]) == False
+    height = getTreeHeight(x, y)
+    return isHiddenBehindAnotherTree(height, easternTrees) == False
 
 def isVisibleWest(x, y):
     if x == 0:
         return True
-    westernTrees = forrest[y]
-    return isHiddenBehindAnotherTree(westernTrees[x], westernTrees[0: x]) == False
+    westernTrees = getWesternTrees(x, y)
+    height = getTreeHeight(x, y)
+    return isHiddenBehindAnotherTree(height, westernTrees) == False
 
 def isVisibleNorth(x, y):
     if y == 0:
         return True
-    northernTrees = getVerticalTrees(x)[0 : y]
-    return isHiddenBehindAnotherTree(forrest[y][x], northernTrees) == False
+    northernTrees = getNorthernTrees(x, y)
+    height = getTreeHeight(x, y)
+    return isHiddenBehindAnotherTree(height, northernTrees) == False
 
 def isVisibleSouth(x, y):
-    if y == len(forrest) - 1:
+    if y == forrestHeight - 1:
         return True
-    southernTrees = getVerticalTrees(x)[y + 1:]
-    return isHiddenBehindAnotherTree(forrest[y][x], southernTrees) == False
+    southernTrees = getSouthernTrees(x, y)
+    height = getTreeHeight(x, y)
+    return isHiddenBehindAnotherTree(height, southernTrees) == False
+
+def isHiddenBehindAnotherTree(height, otherTrees):
+    isVisible = True
+    for tree in otherTrees:
+        if int(tree) >= int(height):
+            isVisible = False
+    return isVisible == False
 
 
-print("forrest is this high: ", len(forrest))
-
-# Top
-# 332003300013411421030123310000325304355245416315402454123304251100551413414301042114403240340202013
-# 130120131034131214123330450045513234151453520504226501101432120555550112535000130014212133321332113
-
-# Bottom
-# 132200122232433121333410132153131541542513264261463154353021340403623114231305043110312440012243111
-# 001320030221212412130340411450015410126162032430413232552634423102222044153052245310344121344231122
-
+print("Tests :)")
 print("Should be visible:")
-print(isVisibleEast(-8, 0))
-print(isVisibleWest(12, 0))
-print(isVisibleNorth(3, 1))
-print(isVisibleSouth(0, len(forrest) - 2))
+print("Is Visible: ", isVisibleEast(-8, 0))
+print("Is Visible: ", isVisibleWest(12, 0))
+print("Is Visible: ", isVisibleNorth(3, 1))
+print("Is Visible: ", isVisibleSouth(0, len(forrest) - 2))
 
 print("Should not be visible:")
-print(isVisibleEast(-3, 1))
-print(isVisibleWest(4, 1))
-print(isVisibleNorth(6, 1))
-print(isVisibleSouth(4, len(forrest) - 2))
+print("Is Visible: ", isVisibleEast(-3, 1))
+print("Is Visible: ", isVisibleWest(4, 1))
+print("Is Visible: ", isVisibleNorth(6, 1))
+print("Is Visible: ", isVisibleSouth(4, len(forrest) - 2))
 
 def isVisible(x, y):
     return isVisibleEast(x, y) or isVisibleWest(x, y) or isVisibleNorth(x, y) or isVisibleSouth(x, y)
 
-height = len(forrest)
-width = len(forrest[0])
-
 visibleTrees = 0
+trees = []
+treescores = []
 
-for i in range(height):
-    for j in range(width):
-        if i == 0 or i == height - 1:
-            visibleTrees += 1
-        elif j == 0 or j == width - 1:
-            visibleTrees += 1
+for i in range(forrestHeight):
+    for j in range(forrestWidth):
+
+        t = Tree(j, i, getTreeHeight(j, i))
+        trees.append(t)
+
+        visible = False
+        if i == 0 or i == forrestHeight - 1 or j == 0 or j == forrestWidth - 1:
+            visible = True
         else:
             if isVisible(j, i):
-                visibleTrees += 1
+                visible = True
+        if visible:
+            visibleTrees += 1
 
+highestScenicScore = 0
+bestScoringTree = {}
+
+for tree in trees:
+    treeScore = tree.getScenicScore()
+    if treeScore > highestScenicScore:
+        highestScenicScore = treeScore
+        bestScoringTree = tree
+
+print("Best scenic score: ", highestScenicScore)
 print("Visible trees: ", visibleTrees)
+
+
