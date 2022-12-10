2eb7d74cc1acf603f4fd66f962d8bf098a45b5f4
Erik Skinnari <erik@skinnari.se>
2022-12-10 21:31:02 +0100

    add old stuff

--- 05/day5.py
+++ 05/day5.py
@@ -1,4 +1,30 @@
+        [Q] [B]         [H]        
+    [F] [W] [D] [Q]     [S]        
+    [D] [C] [N] [S] [G] [F]        
+    [R] [D] [L] [C] [N] [Q]     [R]
+[V] [W] [L] [M] [P] [S] [M]     [M]
+[J] [B] [F] [P] [B] [B] [P] [F] [F]
+[B] [V] [G] [J] [N] [D] [B] [L] [V]
+[D] [P] [R] [W] [H] [R] [Z] [W] [S]
+ 1   2   3   4   5   6   7   8   9 
+ 
+quantity = 0
+source = 0
+destination = 0
+
+move 1 from 4 to 1
+
 input = open("input.txt", "r")
+
+startCrates = []
+
+while True:
+    if (input != '\n'):
+        startCrates.append(input)
+ 
+    else:
+        break
+
 
 
 
