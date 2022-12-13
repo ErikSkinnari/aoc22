a686e224e6021262999377e35d5665f07f8003f8
Erik Skinnari <erik@skinnari.se>
2022-12-14 00:13:09 +0100

    day 9 part 1

--- 09/day9.py
+++ 09/day9.py
@@ -1,5 +1,5 @@
-filecontent = open("testinput.txt", "r")
-#filecontent = open("input.txt", "r")
+#filecontent = open("testinput.txt", "r")
+filecontent = open("input.txt", "r")
 class Vector:
     def __init__(self, x, y ):
         self.x = x
@@ -59,25 +59,40 @@ class Mover:
         deltaY = self.y - self.tailY
 
         print("Delta: ", deltaX, deltaY)
+        changeX = 0
+        if deltaX < 0:
+            changeX = -1
+        elif deltaX > 0:
+            changeX = 1
 
-        if abs(deltaX) > 1 and abs(deltaY) > 1:
-            print("diagonal move!")
-            self.tailX += 1
-            self.tailY += 1
-
-        elif abs(deltaX) > 1:
+        changeY = 0
+        if deltaY < 0:
+            changeY = -1
+        elif deltaY > 0:
+            changeY = 1
+            
+        if abs(deltaX) > 1 and abs(deltaY) == 0:
             print("horisontal move")
-                self.tailX += 1
-            else:
-                self.tailX += -1
-        elif abs(deltaY) > 1:
+            self.tailX += changeX
+        elif abs(deltaY) > 1 and abs(deltaX) == 0:
             print("vertical move")
-            if deltaY > 0:
-                self.tailY += 1
-            else:
-                self.tailY += -1
-
+            self.tailY += changeY
+        elif (abs(deltaX) > 0 and abs(deltaY) > 1) or (abs(deltaX) > 1 and abs(deltaY) > 0):
+            print("diagonal move!")
+            self.tailX += changeX
+            self.tailY += changeY
         print("tail: ", self.tailX, self.tailY)
+
+        for i in range(5):
+            for j in range(6):
+                if self.y == i and self.x == j:
+                    print("H", end="")
+                elif self.tailY == i and self.tailX == j:
+                    print("T", end="")
+                else:
+                    print(".", end="")
+            print()
+
         self.visited.append(Vector(self.tailX, self.tailY))
 
 
